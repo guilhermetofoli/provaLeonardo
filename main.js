29e7e3a67555cecc0089355835a6a4194c3c3dc2
Guilherme Tófoli <guitofoli1806@gmail.com>
2025-04-22 20:13:32 -0300

    Adicionado função para excluir da lista

--- main.js
+++ main.js
@@ -48,3 +48,16 @@ function criaLista() {
     }
     document.getElementById("tabela").innerHTML = tabela;
 }
+
+// Função para excluir nome da lista
+function excluir(linha) {
+    dadosLista.splice(linha - 1, 1);
+    criaLista();
+}
+// Função para editar nome da lista
+function editar(linha) {
+    let tarefa = dadosLista[linha - 1];
+    document.getElementById('nomeTarefa').value = tarefa.descricao;
+    document.getElementById('valorTarefa').value = tarefa.valor;
+    document.getElementById('categoriaTarefa').value = tarefa.categoria;
+}
