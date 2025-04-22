da831503f66f1c9f9e07e3769652fece380f570b
Guilherme Tófoli <guitofoli1806@gmail.com>
2025-04-22 19:31:31 -0300

    Criado função em JS para validar o login e redirecionar para cadastro.html

---
+++ main.js
@@ -0,0 +1,13 @@
+// Implementa acesso para a tela de login
+function acessar(){
+    let loginEmail = document.getElementById('loginEmail').value;
+    let loginSenha = document.getElementById('loginSenha').value;
+
+    //Implementa a estrutura que testa se todos os campos foram preenchidos
+    if(!loginEmail || !loginSenha){
+        alert('Favor preencher todos os campos!');
+        return;
+    }else {
+        window.location.href = 'cadastro.html';
+    }
+}
